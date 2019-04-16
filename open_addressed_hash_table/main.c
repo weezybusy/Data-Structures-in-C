5c4deabafa74bf371c0b41637135039de218bfc3
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-04-16 11:57:59 +0300

    Added tests for ohtbl_remove() and ohtbl_lookup().

--- open_addressed_hash_table/main.c
+++ open_addressed_hash_table/main.c
@@ -14,7 +14,7 @@ void test_ohtbl_lookup(void);
 int h1(const void *key);
 int h2(const void *key);
 int compare(const void *value1, const void *value2);
-void ohtbl_fill(ohtbl_t *htbl, int values[], size_t size);
+void ohtbl_fill(ohtbl_t *htbl, size_t size);
 
 int main(void)
 {
@@ -25,8 +25,8 @@ int main(void)
 void test_ohtbl(void)
 {
         test_ohtbl_insert();
-        //test_ohtbl_remove();
-        //test_ohtbl_lookup();
+        test_ohtbl_remove();
+        test_ohtbl_lookup();
 }
 
 void test_ohtbl_insert(void)
@@ -62,7 +62,7 @@ void test_ohtbl_insert(void)
         for (int i = 0; i < BUCKETS - 1; i++) {
                 assert((ptr = (int *) malloc(sizeof(int))) != NULL);
                 *ptr = i;
-                ohtbl_insert(&htbl, (void *) ptr);
+                assert(ohtbl_insert(&htbl, (void *) ptr) == 0);
                 assert(htbl.size == ++size);
                 tmp = ptr;
                 assert(ohtbl_lookup(&htbl, (void **) &tmp) == 0);
@@ -79,7 +79,7 @@ void test_ohtbl_insert(void)
         for (int i = 0; i < BUCKETS; i++) {
                 assert((ptr = (int *) malloc(sizeof(int))) != NULL);
                 *ptr = i;
-                ohtbl_insert(&htbl, (void *) ptr);
+                assert(ohtbl_insert(&htbl, (void *) ptr) == 0);
                 assert(htbl.size == ++size);
                 tmp = ptr;
                 assert(ohtbl_lookup(&htbl, (void **) &tmp) == 0);
@@ -107,11 +107,81 @@ void test_ohtbl_insert(void)
 
 void test_ohtbl_remove(void)
 {
+        ohtbl_t htbl;
+        int *ptr;
+        int *tmp;
+        int val;
+
+        // Check removing from the empty table.
+        ohtbl_init(&htbl, BUCKETS, h1, h2, compare, free);
+        assert(htbl.size == 0);
+        assert((ptr = (int *) malloc(sizeof(int))) != NULL);
+        val = 11;
+        *ptr = val;
+        tmp = ptr;
+        assert(ohtbl_remove(&htbl, (void **) &tmp) == -1);
+        free(ptr);
+        ohtbl_destroy(&htbl);
+
+        // Remove every element from the full table.
+        ohtbl_init(&htbl, BUCKETS, h1, h2, compare, free);
+        assert(htbl.size == 0);
+        ptr = NULL;
+        tmp = NULL;
+        ohtbl_fill(&htbl, BUCKETS);
+        assert(htbl.size == BUCKETS);
+        for (int i = 0; i < BUCKETS; i++) {
+                assert((ptr = (int *) malloc(sizeof(int))) != NULL);
+                *ptr = i;
+                tmp = ptr;
+                assert(ohtbl_remove(&htbl, (void **) &tmp) == 0);
+                assert(htbl.size == (size_t) BUCKETS - i - 1);
+                assert(*tmp == i);
+                free(ptr);
+                free(tmp);
+        }
+        assert(htbl.size == 0);
+        ohtbl_destroy(&htbl);
+
         printf("%-30s ok\n", __func__);
 }
 
 void test_ohtbl_lookup(void)
 {
+        ohtbl_t htbl;
+        int *ptr;
+        int *tmp;
+        int val;
+
+        // Check lookup of the element in the empty table.
+        ohtbl_init(&htbl, BUCKETS, h1, h2, compare, free);
+        assert(htbl.size == 0);
+        assert((ptr = (int *) malloc(sizeof(int))) != NULL);
+        val = 11;
+        *ptr = val;
+        tmp = ptr;
+        assert(ohtbl_lookup(&htbl, (void **) &tmp) == -1);
+        free(ptr);
+        ohtbl_destroy(&htbl);
+
+        // Lookup of every element in the full table.
+        ohtbl_init(&htbl, BUCKETS, h1, h2, compare, free);
+        assert(htbl.size == 0);
+        ptr = NULL;
+        tmp = NULL;
+        ohtbl_fill(&htbl, BUCKETS);
+        assert(htbl.size == BUCKETS);
+        for (int i = 0; i < BUCKETS; i++) {
+                assert((ptr = (int *) malloc(sizeof(int))) != NULL);
+                *ptr = i;
+                tmp = ptr;
+                assert(ohtbl_lookup(&htbl, (void **) &tmp) == 0);
+                assert(htbl.size == BUCKETS);
+                assert(*tmp == i);
+                free(ptr);
+        }
+        ohtbl_destroy(&htbl);
+
         printf("%-30s ok\n", __func__);
 }
 
@@ -144,13 +214,12 @@ int compare(const void *value1, const void *value2)
         }
 }
 
-void ohtbl_fill(ohtbl_t *htbl, int values[], size_t size)
+void ohtbl_fill(ohtbl_t *htbl, size_t size)
 {
         int *ptr = NULL;
         for (size_t i = 0; i < size; i++) {
-                ptr = (int *) malloc(sizeof(int));
-                assert(ptr != NULL);
-                *ptr = values[i];
+                assert((ptr = (int *) malloc(sizeof(int))) != NULL);
+                *ptr = i;
                 assert(ohtbl_insert(htbl, (void *) ptr) != -1);
         }
 }
