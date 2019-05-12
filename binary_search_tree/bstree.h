e9624b3512a9a9007aad3c70a3151d870a205586
weezybusy <vitaliy.pisnya@gmail.com>
2019-05-12 08:35:50 +0000

    Started working on Binary Search Tree.

---
+++ binary_search_tree/bstree.h
@@ -0,0 +1,13 @@
+#ifndef BSTREE_H
+#define BSTREE_H
+
+#include "btree.h"
+
+typedef bstree {
+
+} bstree_t;
+
+/* Public Interface. ------------------------------------------------------ */
+/* Macros. ---------------------------------------------------------------- */
+
+#endif
---
+++ binary_search_tree/main.c
@@ -0,0 +1,52 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bstree.h"
+
+void test_bstree(void);
+void test_bstree_init(void);
+void test_bstree_destroy(void);
+void test_bstree_insert(void);
+void test_bstree_remove(void);
+void test_bstree_lookup(void);
+
+int main(void)
+{
+        test_bstree(void);
+        return EXIT_SUCCESS;
+}
+
+void test_bstree(void)
+{
+        test_bstree_init();
+        test_bstree_destroy();
+        test_bstree_insert();
+        test_bstree_remove();
+        test_bstree_lookup();
+}
+
+void test_bstree_init(void)
+{
+        printf("%-30s ok\n", __func__);
+}
+
+void test_bstree_destroy(void)
+{
+        printf("%-30s ok\n", __func__);
+}
+
+void test_bstree_insert(void)
+{
+        printf("%-30s ok\n", __func__);
+}
+
+void test_bstree_remove(void)
+{
+        printf("%-30s ok\n", __func__);
+}
+
+void test_bstree_lookup(void)
+{
+        printf("%-30s ok\n", __func__);
+}
