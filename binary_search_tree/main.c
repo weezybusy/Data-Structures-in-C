7a66a14489553051934bb8b0672e14e5b4e8e0fb
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-08 10:50:49 +0300

    Started write tests for a Binary Search Tree.

--- binary_search_tree/main.c
+++ binary_search_tree/main.c
@@ -10,6 +10,9 @@ void test_bstree_destroy(void);
 void test_bstree_insert(void);
 void test_bstree_remove(void);
 
+static int compare(const void *value1, const void *value2);
+//static void fill(bstree_t *tree, int values[], size_t n);
+
 int main(void)
 {
         test_bstree();
@@ -19,27 +22,72 @@ int main(void)
 void test_bstree(void)
 {
         test_bstree_init();
-        test_bstree_destroy();
+        //test_bstree_destroy();
         test_bstree_insert();
-        test_bstree_remove();
+        //test_bstree_remove();
 }
 
 void test_bstree_init(void)
 {
-        printf("%-30s ok\n", __func__);
-}
+        bstree_t tree;
+        bstree_init(&tree, compare, free);
+        assert(tree.size == 0);
+        assert(tree.compare == compare);
+        assert(tree.destroy == free);
+        assert(tree.root == NULL);
 
-void test_bstree_destroy(void)
-{
         printf("%-30s ok\n", __func__);
 }
 
 void test_bstree_insert(void)
 {
+        bstree_t tree;
+        bstree_init(&tree, compare, free);
+
+        assert(bstree_size(&tree) == 0);
+
+        int *data = (int *) malloc(sizeof(int));
+        assert(data != NULL);
+        *data = 5;
+        bstree_insert(&tree, (void *) data);
+        assert(bstree_size(&tree) == 1);
+        // Add more values.
+
+        bstree_destroy(&tree);
+
         printf("%-30s ok\n", __func__);
 }
 
 void test_bstree_remove(void)
 {
         printf("%-30s ok\n", __func__);
+}
+
+void test_bstree_destroy(void)
+{
+        bstree_t tree;
+        bstree_init(&tree, compare, free);
+        //fill(&tree, [values], size);
+
+        bstree_destroy(&tree);
+        printf("%-30s ok\n", __func__);
+}
+
+//static void fill(bstree_t *tree, int values[], size_t n)
+//{
+//        return;
+//}
+
+static int compare(const void *value1, const void *value2)
+{
+        int v1 = *(int *) value1;
+        int v2 = *(int *) value2;
+
+        if (v1 < v2) {
+                return -1;
+        } else if (v1 > v2) {
+                return 1;
+        } else {
+                return 0;
+        }
 }
