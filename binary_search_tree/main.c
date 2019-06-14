338ea9a7df3bc39987e5793642b452d051793b57
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-14 11:12:05 +0300

    Finished tests for Binary Search Tree.

--- binary_search_tree/main.c
+++ binary_search_tree/main.c
@@ -55,7 +55,7 @@ void test_bstree_insert(void)
         value = 5;
         *data = value;
         // Check return value is correct.
-        assert(bstree_insert(&tree, (void *) data) == 0);
+        assert(bstree_insert(&tree, data) == 0);
         tree_size++;
         // Check tree size.
         assert(bstree_size(&tree) == tree_size);
@@ -68,7 +68,7 @@ void test_bstree_insert(void)
         assert(data != NULL);
         value = 4;
         *data = value;
-        assert(bstree_insert(&tree, (void *) data) == 0);
+        assert(bstree_insert(&tree, data) == 0);
         tree_size++;
         assert(bstree_size(&tree) == tree_size);
         assert(*(int *) ((avlnode_t *) btree_data(btree_left(btree_root(&tree))))->data == value);
@@ -79,7 +79,7 @@ void test_bstree_insert(void)
         assert(data != NULL);
         value = 6;
         *data = value;
-        assert(bstree_insert(&tree, (void *) data) == 0);
+        assert(bstree_insert(&tree, data) == 0);
         tree_size++;
         assert(bstree_size(&tree) == tree_size);
         assert(*(int *) ((avlnode_t *) btree_data(btree_right(btree_root(&tree))))->data == value);
@@ -91,10 +91,11 @@ void test_bstree_insert(void)
         assert(data != NULL);
         value = 6;
         *data = value;
-        assert(bstree_insert(&tree, (void *) data) == 1);
+        assert(bstree_insert(&tree, data) == 1);
         assert(bstree_size(&tree) == tree_size);
         free(data);
 
+        data = NULL;
         bstree_destroy(&tree);
 
         printf("%-30s ok\n", __func__);
@@ -106,19 +107,24 @@ void test_bstree_lookup(void)
         bstree_init(&tree, compare, free);
         int values[] = { 9, 123, 41, 59, 7, 6, 1, 34 };
         const size_t tree_size = sizeof values / sizeof values[0];
+        void *data = NULL;
 
         fill(&tree, values, tree_size);
         assert(tree.size == tree_size);
 
-        int *data = NULL;
+        // Check lookup of data not in the tree.
+        int value = 1000;
+        data = &value;
+        assert(bstree_lookup(&tree, &data) == -1);
+
+        // Check lookup of data in the tree.
         for (size_t i = 0; i < tree_size; i++) {
                 data = &values[i];
-                assert(bstree_lookup(&tree, (void **) &data) == 0);
-                assert(*data == values[i]); 
+                assert(bstree_lookup(&tree, &data) == 0);
+                assert(*(int *) data == values[i]); 
         }
+        data = NULL;
         bstree_destroy(&tree);
-
-        // TODO: check lookup of data not in the tree.
 
         printf("%-30s ok\n", __func__);
 }
@@ -126,14 +132,13 @@ void test_bstree_lookup(void)
 void test_bstree_remove(void)
 {
         bstree_t tree;
-        int *data;
-        const int values[] = { 9, 123, 41, 59, 7, 6, 1, 34 };
+        int *data = NULL;
+        int values[] = { 9, 123, 41, 59, 7, 6, 1, 34 };
         const size_t tree_size = sizeof values / sizeof values[0];
 
         // Check removing from the empty tree.
         bstree_init(&tree, compare, free);
-        int value = 7;
-        assert(bstree_remove(&tree, &value) == -1);
+        assert(bstree_remove(&tree, &values[0]) == -1);
         bstree_destroy(&tree);
 
         // Check removing an element which is not in the tree.
@@ -145,19 +150,24 @@ void test_bstree_remove(void)
         *data = 100;
         assert(bstree_remove(&tree, data) == -1);
         free(data);
+        data = NULL;
         bstree_destroy(&tree);
 
         // Check removing elements from the tree which is not empty.
         bstree_init(&tree, compare, free);
         fill(&tree, values, tree_size);
         assert(bstree_size(&tree) == tree_size);
-
-        // TODO: use bstree_lookup() for checking removed data.
-        for (int i = tree_size - 1; i >= 0; i--) {
-                assert(bstree_remove(&tree, &values[i]) == 0);
+        for (size_t i = 0; i < tree_size; i++) {
+                data = &values[i];
+                // Check the data has been removed successfully.
+                assert(bstree_remove(&tree, data) == 0);
+                // Check there is no data after repeated search.
+                assert(bstree_lookup(&tree, (void **) &data) == -1);
         }
+        // Check the size is the same (removed elements are marked as hidden).
+        assert(bstree_size(&tree) == tree_size);
+        data = NULL;
         bstree_destroy(&tree);
-
 
         printf("%-30s ok\n", __func__);
 }
@@ -166,7 +176,7 @@ void test_bstree_destroy(void)
 {
         bstree_t tree;
         bstree_init(&tree, compare, free);
-        const int values[] = { 5,4,1,7,6,8 };
+        const int values[] = { 5, 4, 1, 7, 6, 8 };
         const size_t tree_size = sizeof values / sizeof values[0];
         fill(&tree, values, tree_size);
         assert(bstree_size(&tree) == tree_size);
@@ -184,6 +194,7 @@ static void fill(bstree_t *tree, const int values[], const size_t n)
                 *data = values[i];
                 bstree_insert(tree, (void *) data);
         }
+        data = NULL;
 }
 
 static int compare(const void *value1, const void *value2)
