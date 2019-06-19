93ef18f9a72f5641236c2415ee881bffc911f6be
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-19 12:02:46 +0300

    Added heap_extract().

--- heap/heap.c
+++ heap/heap.c
@@ -5,7 +5,7 @@
 
 /* Define private macros used by the heap implementation. ----------------- */
 
-#define heap_parent(npos) ((int ) (((npos) - 1) / 2))
+#define heap_parent(npos) ((int) (((npos) - 1) / 2))
 
 #define heap_left(npos) (((npos) * 2) + 1)
 
@@ -42,8 +42,8 @@ void heap_destroy(heap_t *heap)
 int heap_insert(heap_t *heap, const void *data)
 {
         void *temp;
-        int ipos;
-        int ppos;
+        size_t ipos;
+        size_t ppos;
 
         // Allocate storage for the node.
         temp = (void **) realloc(heap->tree, (heap_size(heap) + 1) * sizeof (void *));
@@ -79,5 +79,78 @@ int heap_insert(heap_t *heap, const void *data)
 
 int heap_extract(heap_t *heap, void **data)
 {
+        void *save;
+        void *temp;
+        size_t ipos;
+        size_t lpos;
+        size_t rpos;
+        size_t mpos;
+
+        // Do not allow extraction from an empty heap.
+        if (heap_size(heap) == 0) {
+                return -1;
+        }
+
+        // Extract the node at the top of the heap.
+        *data = heap->tree[0];
+
+        // Adjust the storage used by the heap.
+        save = heap->tree[heap_size(heap) - 1];
+
+        if (heap_size(heap) - 1 > 0) {
+                temp = (void **) realloc(heap->tree, (heap_size(heap) - 1) * sizeof (void *));
+                if (temp == NULL) {
+                        return -1;
+                } else {
+                        heap->tree = temp;
+                }
+
+                // Adjust the size of the heap to account for the extracted node.
+                heap->size--;
+        } else {
+                // Manage the heap when extracting the last node.
+                free(heap->tree);
+                heap->tree = NULL;
+                heap->size = 0;
+                return 0;
+        }
+
+        // Copy the last node to the top.
+        heap->tree[0] = save;
+
+        // Heapify the tree by pushing the contents of the new top downward.
+        ipos = 0;
+
+        while (1) {
+                // Select the child to swap with the current node.
+                lpos = heap_left(ipos);
+                rpos = heap_right(ipos);
+
+                if (lpos < heap_size(heap) &&
+                                heap->compare(heap->tree[lpos], heap->tree[ipos]) > 0) {
+                        mpos = lpos;
+                } else {
+                        mpos = ipos;
+                }
+
+                if (rpos < heap_size(heap) &&
+                                heap->compare(heap->tree[rpos], heap->tree[ipos]) < 0) {
+                        mpos = rpos;
+                }
+
+                // When mpos is ipos, the heap property has been restored.
+                if (mpos == ipos) {
+                        break;
+                } else {
+                        // Swap the contents of the current node and the selected child.
+                        temp = heap->tree[mpos];
+                        heap->tree[mpos] = heap->tree[ipos];
+                        heap->tree[ipos] = temp;
+
+                        // Move down one level in the tree to continue heapifying.
+                        ipos = mpos;
+                }
+        }
+
         return 0;
 }
