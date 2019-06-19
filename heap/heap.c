2a5d07d6454f631649f151d5d7d659654b3d5f60
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-19 11:34:42 +0300

    Added heap_insert().

--- heap/heap.c
+++ heap/heap.c
@@ -41,6 +41,39 @@ void heap_destroy(heap_t *heap)
 
 int heap_insert(heap_t *heap, const void *data)
 {
+        void *temp;
+        int ipos;
+        int ppos;
+
+        // Allocate storage for the node.
+        temp = (void **) realloc(heap->tree, (heap_size(heap) + 1) * sizeof (void *));
+        if (temp == NULL) {
+                return -1;
+        } else {
+                heap->tree = temp;
+        }
+
+        // Insert the node after the last node.
+        heap->tree[heap_size(heap)] = (void *) data;
+
+        // Heapify the tree by pushing the contents of the new node upward.
+        ipos = heap_size(heap);
+        ppos = heap_parent(ipos);
+
+        while (ipos > 0 && heap->compare(heap->tree[ppos], heap->tree[ipos]) < 0) {
+                // Swap the contents of the current node and its parent.
+                temp = heap->tree[ppos];
+                heap->tree[ppos] = heap->tree[ipos];
+                heap->tree[ipos] = temp;
+
+                // Move up one level in the tree to continue heapifying.
+                ipos = ppos;
+                ppos = heap_parent(ipos);
+        }
+
+        // Adjust the size of the heap to account for the inserted node.
+        heap->size++;
+
         return 0;
 }
 
